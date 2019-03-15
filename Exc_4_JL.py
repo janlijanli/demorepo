b5035ca93003e67755579806bd4346317381fc64
Jan Liechti <jan.liechti93@gmail.com>
2019-03-15 15:26:13 +0100

    test_comit_1

---
+++ Exc_4_JL.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# define Function, easy solution with pandas
+def calc_stat(p):
+    p_dat = pd.read_csv(p, delim_whitespace=True)
+    pmax = p_dat.loc[:, 'rre150d0'].max()
+    pmin = p_dat.loc[:, 'rre150d0'].min()
+    pmean = p_dat.loc[:, 'rre150d0'].mean()
+    return [pmax, pmin, pmean]  # returns a list
+
+path = r'C:\Users\Jan Liechti\Google Drive\UNI\FS19\Geographie\Gedatenanalyse_u_Modellierung\02 Intro Python\precip_data.txt'  # path to Textfile
+results = calc_stat(path)
+print('maximum precipitation: {} \nminimum precipitation: {}\nmean precipitation: {}'.format(results[0], results[1], round(results[2], 1)))
